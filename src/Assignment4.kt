4bdb152cfb7c9668b6129cd3084d3748019221f0
jangkunsuguri <jsuguri@my.bridgeport.edu>
2020-10-22 19:00:44 -0400

    no message

--- src/Assignment4.kt
+++ src/Assignment4.kt
@@ -49,21 +49,30 @@ fun compare(e: String, L: List<String>, comp: (String, String)-> Boolean): List<
 
 //quicksort
 fun quicksort(L: List<Int>, comp:(Int, Int)-> Boolean): List<Int>{
+    //base case for the recursive function
     return if (L.size<=1)
         L
     else {
+        //sets the first element of the list as the pivot
         val pivot = L[0]
+        //returns the list that for the left side of the pivot (elements less than pivot if x<y greater if x>y)
         val left = partition(pivot, L.subList(1, L.size), comp)
+        //returns the list that for the right side of the pivot (elements greater than pivot if x<y less if x>y)
         val right = partition(pivot, L.subList(1, L.size)) { x, y-> !comp(x,y)}
+        //recursive call after setting pivot into a correct index
         quicksort(left, comp) + pivot + quicksort(right, comp)
     }
 }
 
+//returns the list containing the elements that pass through the condition specified in lambda
 fun partition( e: Int, L: List<Int>, comp: (Int, Int)->(Boolean)): List<Int>{
+    //base case for the recursive function
     if (L.isEmpty())
         return listOf()
+    //adds to the list if condition is met
     return if (comp(L[0],e))
         listOf(L[0]) + partition(e, L.subList(1,L.size), comp)
+    //skips the element if condition is not met
     else
         partition(e, L.subList(1, L.size), comp)
 }
