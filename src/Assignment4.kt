996947186eede3d2d40ac6308de63cd2f331604b
KyungminUm <snownaul2@gmail.com>
2020-10-22 03:19:57 -0400

    - comp lambda, descending quicksort

--- src/Assignment4.kt
+++ src/Assignment4.kt
@@ -6,61 +6,64 @@ fun main(args: Array<String>){
     println(more("no", listOf("not", "yes", "a", "hello")))
 
     println("fun compare (less):")
-    println(compare("no", listOf("not", "yes", "a", "hello"), {x: String, y:String -> x.length < y.length}))
+    println(compare("no", listOf("not", "yes", "a", "hello")) { x: String, y:String -> x.length < y.length})
 
     println("fun compare (more):")
-    println(compare("no", listOf("not", "yes", "a", "hello"), {x: String, y:String -> x.length > y.length}))
+    println(compare("no", listOf("not", "yes", "a", "hello")) { x: String, y:String -> x.length > y.length})
 
     println("Quicksort Ascending Order:")
-    println(quicksort(listOf(2,5,3,1,4)))
+    println(quicksort(listOf(2,5,3,1,4)) { x:Int, y:Int-> x < y})
+
+    println("Quicksort Descending Order:")
+    println(quicksort(listOf(2,5,3,1,4)) { x:Int, y:Int-> x > y})
 }
 //1.a. less function
 fun less(e: String, L: List<String>): List<String>{
     if (L.isEmpty())
         return listOf()
-    if (L[0].length < e.length)
-        return listOf(L[0]) + less(e, L.subList(1,L.size))
+    return if (L[0].length < e.length)
+        listOf(L[0]) + less(e, L.subList(1,L.size))
     else
-        return (less(e, L.subList(1,L.size)))
+        (less(e, L.subList(1,L.size)))
 }
 
 //1.b. more function
 fun more(e: String, L: List<String>): List<String>{
     if (L.isEmpty())
         return listOf()
-    if (L[0].length > e.length)
-        return listOf(L[0]) + more(e, L.subList(1,L.size))
+    return if (L[0].length > e.length)
+        listOf(L[0]) + more(e, L.subList(1,L.size))
     else
-        return (more(e, L.subList(1,L.size)))
+        (more(e, L.subList(1,L.size)))
 }
 
 //1.c. compare function
 fun compare(e: String, L: List<String>, comp: (String, String)-> Boolean): List<String>{
     if (L.isEmpty())
         return listOf()
-    if (comp(L[0],e))
-        return listOf(L[0]) + compare(e, L.subList(1,L.size), comp)
+    return if (comp(L[0],e))
+        listOf(L[0]) + compare(e, L.subList(1,L.size), comp)
     else
-        return compare(e, L.subList(1,L.size), comp)
+        compare(e, L.subList(1,L.size), comp)
 }
 
 //quicksort
-fun quicksort(L: List<Int>): List<Int>{
-    if (L.size<=1)
-        return L
+fun quicksort(L: List<Int>, comp:(Int, Int)-> Boolean): List<Int>{
+    return if (L.size<=1)
+        L
     else {
         val pivot = L[0]
-        val left = partition(pivot, L.subList(1, L.size), {x:Int, y:Int-> x < y})
-        val right = partition(pivot, L.subList(1, L.size), {x:Int, y:Int-> x > y})
-        return quicksort(left) + pivot + quicksort(right)
+        val left = partition(pivot, L.subList(1, L.size), comp)
+        val right = partition(pivot, L.subList(1, L.size)) { x, y-> !comp(x,y)}
+        quicksort(left, comp) + pivot + quicksort(right, comp)
     }
 }
 
-fun partition( e: Int, L: List<Int>, comp: (Int, Int) -> Boolean): List<Int>{
+fun partition( e: Int, L: List<Int>, comp: (Int, Int)->(Boolean)): List<Int>{
     if (L.isEmpty())
         return listOf()
-    if (comp(L[0],e))
-        return listOf(L[0]) + partition(e, L.subList(1,L.size), comp)
+    return if (comp(L[0],e))
+        listOf(L[0]) + partition(e, L.subList(1,L.size), comp)
     else
-        return partition(e, L.subList(1, L.size), comp)
+        partition(e, L.subList(1, L.size), comp)
 }
