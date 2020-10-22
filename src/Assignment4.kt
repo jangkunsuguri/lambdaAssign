1543a3b343295aa29b5bc50e13805c045b2178d8
jangkunsuguri <jsuguri@my.bridgeport.edu>
2020-10-21 22:48:36 -0400

    no message

--- src/Assignment4.kt
+++ src/Assignment4.kt
@@ -12,12 +12,12 @@ fun main(args: Array<String>){
     println(compare("no", listOf("not", "yes", "a", "hello"), {x: String, y:String -> x.length > y.length}))
 
     println("Quicksort Ascending Order:")
-    println(quicksort(listOf(2,5,3,1,4), {x:Int, y:Int-> x < y}))
+    println(quicksort(listOf(2,5,3,1,4)))
 
-    println("Quicksort Descending Order: ")
-    println(quicksort(listOf(2,5,3,1,4), {x:Int, y:Int-> x > y}))
+/*    println("Quicksort Descending Order: ")
+    println(quicksort(listOf(2,5,3,1,4)))*/
 }
-//no. 1 less function
+//1.a. less function
 fun less(e: String, L: List<String>): List<String>{
     if (L.isEmpty())
         return listOf()
@@ -27,7 +27,7 @@ fun less(e: String, L: List<String>): List<String>{
         return (less(e, L.subList(1,L.size)))
 }
 
-//no. 2 more function
+//1.b. more function
 fun more(e: String, L: List<String>): List<String>{
     if (L.isEmpty())
         return listOf()
@@ -37,7 +37,7 @@ fun more(e: String, L: List<String>): List<String>{
         return (more(e, L.subList(1,L.size)))
 }
 
-//no. 3 compare function
+//1.c. compare function
 fun compare(e: String, L: List<String>, comp: (String, String)-> Boolean): List<String>{
     if (L.isEmpty())
         return listOf()
@@ -48,30 +48,32 @@ fun compare(e: String, L: List<String>, comp: (String, String)-> Boolean): List<
 }
 
 //quicksort
-fun quicksort(L: List<Int>, comp: (x:Int, y: Int) -> Boolean): List<Int>{
+fun quicksort(L: List<Int>): List<Int>{
     if (L.size<=1)
         return L
     else {
         val pivot = L[0]
-        val left = leftpartition(pivot, L.subList(1, L.size), comp)
-        val right = rightpartition(pivot, L.subList(1, L.size), comp)
-        return quicksort(left, comp) + pivot + quicksort(right, comp)
+        val left = partition(pivot, L.subList(1, L.size), {x:Int, y:Int-> x < y})
+        val right = partition(pivot, L.subList(1, L.size), {x:Int, y:Int-> x > y})
+        return quicksort(left) + pivot + quicksort(right)
     }
 }
 
-fun leftpartition(e: Int, L: List<Int>, comp: (x: Int, y: Int) -> Boolean): List<Int>{
+fun partition( e: Int, L: List<Int>, comp: (Int, Int) -> Boolean): List<Int>{
     if (L.isEmpty())
         return listOf()
     if (comp(L[0],e))
-        return listOf(L[0]) + leftpartition(e, L.subList(1,L.size), comp)
+        return listOf(L[0]) + partition(e, L.subList(1,L.size), comp)
     else
-        return leftpartition(e, L.subList(1, L.size), comp)
+        return partition(e, L.subList(1, L.size), comp)
 }
-fun rightpartition(e: Int, L: List<Int>, comp: (x: Int, y: Int) -> Boolean): List<Int>{
+
+/*
+fun rightpartition(e: Int, L: List<Int>, comp: (Int, Int) -> Boolean): List<Int>{
     if (L.isEmpty())
         return listOf()
     if (!comp(L[0],e))
         return listOf(L[0]) + rightpartition(e, L.subList(1,L.size), comp)
     else
         return rightpartition(e, L.subList(1, L.size), comp)
-}
+}*/
